f09cbea70633d378af3e076017d4cdc1ddc2a61c
Isuru <isuru.akalanka4043@gmail.com>
2024-07-04 23:59:33 +0530

    updated newsletter.css

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -2,7 +2,16 @@ import React, { Component } from "react";
 import "./NewsLetter.css";
 
 const NewsLetter = () => {
-  return <div className="newsletter"></div>;
+  return (
+    <div className="newsletter">
+      <h1>Get Exclusive Offers on your Email</h1>
+      <p>Subscribe to our newsletter and stay updated</p>
+      <div>
+        <input type="email" placeholder="Your Email id" />
+        <button>Subscribe</button>
+      </div>
+    </div>
+  );
 };
 
 export default NewsLetter;
