47cf825473a5ddd084e5b287a280efc1a957ce74
Isuru <isuru.akalanka4043@gmail.com>
2024-08-08 23:58:57 +0530

    created the login-signup page

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import "./CSS/LoginSignup.css";
 
 const LoginSignup = () => {
-  return <></>;
+  return (
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Your name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          Already have an account? <span>Login</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>
+            I agree to the <span>Terms of Service</span> and{" "}
+            <span>Privacy Policy</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginSignup;
