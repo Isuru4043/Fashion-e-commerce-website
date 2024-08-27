fbb7b6d170f9742c9111be5fee1ca7e749b629fc
Isuru <isuru.akalanka4043@gmail.com>
2024-08-27 21:06:17 +0530

    created API for getting all products from the database

--- Backend/index.js
+++ Backend/index.js
@@ -131,6 +131,13 @@ app.post("/removeproduct", async (req, res) => {
   });
 });
 
+//Creating API for getting all products
+app.get("/allproducts", async (req, res) => {
+  let products = await Product.find({});
+  console.log("All products received");
+  res.send(products);
+});
+
 app.listen(port, (error) => {
   if (!error) {
     console.log(
